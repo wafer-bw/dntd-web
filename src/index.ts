15c8fbab3a729f2fda61f18b3f906eb6e1942043
wafer-bw <ben.jz.wakeford@gmail.com>
2020-06-24 15:34:59 -0400

    minor reordering for cleaner appearance

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import m from "mithril"
 import { app, about } from "./components"
 import { Journal, Search, Syncer, ServiceWorker, Refines } from "./classes"
 
-export const serviceWorker = new ServiceWorker()
 export const syncer = new Syncer()
 export const search = new Search()
 export const refines = new Refines()
 export const journal = new Journal()
+export const serviceWorker = new ServiceWorker()
 
 let root = document.getElementById("root")
 
