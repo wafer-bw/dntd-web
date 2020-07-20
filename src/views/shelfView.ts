98461e138bd4c3959f0e2efbc6422b79a6b7e913
wafer-bw <ben.jz.wakeford@gmail.com>
2020-07-20 10:43:29 -0400

    add breadcrumb component

--- src/views/shelfView.ts
+++ src/views/shelfView.ts
@@ -1,10 +1,11 @@
 import m from "mithril"
-import { googleComponent, journalsComponent, syncStateComponent } from "./components"
+import { googleComponent, journalsComponent, syncStateComponent, breadcrumbComponent } from "./components"
 
 export function shelfView() {
 
     function view() {
         return m("#shelf", [
+            m(breadcrumbComponent),
             m(googleComponent),
             m(syncStateComponent),
             m(journalsComponent),
