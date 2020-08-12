57ab7532075ba015042774d902c8e407a8dc880a
wafer-bw <ben.jz.wakeford@gmail.com>
2020-08-12 16:22:39 -0400

    breadcrumb visibility improvement

--- src/components/breadcrumbComponent.ts
+++ src/components/breadcrumbComponent.ts
@@ -1,9 +1,11 @@
 import m from "mithril"
+import { googleModel } from ".."
 import { urlController } from "../controllers"
 
 export function breadcrumbComponent() {
 
     function view() {
+        if (!googleModel.isSignedIn) return
         return m("#breadcrumb", urlController.getBreadcrumbTrail())
     }
 
